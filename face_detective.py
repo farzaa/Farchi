7b3d602842a4b76ba55c35d9b7b22dfe80b36920
Richard Wales <richard_wales@knights.ucf.edu>
2017-02-18 16:03:17 -0500

    Texting detection :)

--- face_detective.py
+++ face_detective.py
@@ -12,6 +12,12 @@ time1 = None
 time2 = None
 iteration = 1
 
+txt_time1 = None
+txt_time2 = None
+max_txting = 3 # How many times you can be caught
+times_txting = 0 # How many times you HAVE been caught
+txt_wait = 1 # How many seconds to wait until next text check
+
 def get_faces(gray):
     faces = faceCascade.detectMultiScale(gray, 1.05, 3, minSize=(100,100))
     return faces
@@ -29,7 +35,12 @@ def mark_faces(frame):
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        txt = is_texting((x,y,w,h), frame)
         num_eyes = mark_eyes(gray, frame, x, y, w, h)
+
+        if txt:
+            alert_driver(texting=True)
+        
 
     if num_eyes <= 0:
         print("No eyes")
@@ -46,17 +57,48 @@ def mark_faces(frame):
         time2 = None
         iteration = 1
 
-def alert_driver():
+def is_texting(face, frame):
+    global txt_time1
+    global txt_time2
+    global times_txting
+    global max_txting
+
+    print("Times texting: " + str(times_txting))
+
+    txt_time2 = time.time()
+
+    if txt_time1 and (txt_time2 - txt_time1 < txt_wait):
+        return False
+
+    fx, fy, fw, fh = face
+    starty = 90
+    height, width, channels = frame.shape
+    cv2.rectangle(frame, (0, starty), (width, starty + 40), (0, 0, 255), 2)
+
+    if fy > starty:
+        txt_time1 = time.time()
+        times_txting += 1
+        if times_txting >= max_txting:
+            times_txting = 0
+            return True
+
+    return False
+
+def alert_driver(texting=False):
     global iteration
     
+    if texting:
+        subprocess.run(['say', '"Get off your phone!"'])
+        return
+
     if iteration == 1:
         subprocess.run(['say', '"Are you okay?"'])
     elif iteration == 2:
-        subprocess.run(['say', '"Richie, are you okay?"'])
+        subprocess.run(['say', '"Richie, are you sure you are okay?"'])
     elif iteration == 3:
-        subprocess.run(['say', '"Holy Shit, are you okay? Please be okay!"'])
+        subprocess.run(['say', '"Holy guacamole, are you okay?"'])
     elif iteration == 4:
-        subprocess.run(['say', 'Motherfucker, wake up!'])
+        subprocess.run(['say', 'You are going to die! Wake up!'])
     elif iteration >= 5:
         please = "please " * (iteration-4)
         subprocess.run(['say', please + " are you okay?"])
