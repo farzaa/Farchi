fa8bbfa0953d37e056f195333e8cc61e74528593
Farzain Majeed <contactclothinghousenow@gmail.com>
2017-02-18 14:00:10 -0500

    added code for dual cameras

--- detective.py
+++ detective.py
@@ -8,15 +8,20 @@ class Detective():
 
     def track_faces(self):
         video_capture = cv2.VideoCapture(0)
+        video_capture_front = cv2.VideoCapture(1)
 
         while True:
             # Capture frame-by-frame
             ret, frame = video_capture.read()
-            frame = cv2.resize(frame,None,fx=0.4, fy=0.4, interpolation = cv2.INTER_CUBIC)
+            ret_front, frame_front = video_capture_front.read()
+            frame = cv2.resize(frame, None,fx=0.4, fy=0.4, interpolation = cv2.INTER_CUBIC)
+            frame_front = cv2.resize(frame_front, None,fx=0.4, fy=0.4, interpolation = cv2.INTER_CUBIC)
             mark_faces(frame)
 
             # Display the resulting frame
             cv2.imshow('Video', frame)
+
+            cv2.imshow('Video_Front', frame_front)
 
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -24,6 +29,7 @@ class Detective():
             
         # When everything is done, release the capture
         video_capture.release()
+        video_capture_front.release()
         cv2.destroyAllWindows()
 
 if __name__ == "__main__":
